5cebf115408a7dec9fdbc962ef330c5128f144f9
Pedro henrique Ramos <pedrohscramos@gmail.com>
2022-06-15 21:45:15 -0300

    outro ajuste de cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,8 +7,9 @@ app.use(express.json());
 app.use(cors());
 app.use(linksRouter);
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-type, Accept, privatekey')
     next();
 });
 
