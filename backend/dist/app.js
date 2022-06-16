40c4a9c5f8a01a25ccbcea651ae1119bfe0d9875
Pedro henrique Ramos <pedrohscramos@gmail.com>
2022-06-16 00:05:01 -0300

    cors

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -8,13 +8,8 @@ const links_1 = __importDefault(require("./routes/links"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((req, res, next) => {
-    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-    //Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
-    app.use((0, cors_1.default)());
-    next();
-});
+app.use((0, cors_1.default)({
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
 app.use(links_1.default);
 exports.default = app;
