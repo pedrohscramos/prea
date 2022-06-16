0353a25da07e4b8b18c35d46a978d2b3e8a4485f
Pedro henrique Ramos <pedrohscramos@gmail.com>
2022-06-15 22:25:27 -0300

    options do cors

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -10,4 +10,10 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use(links_1.default);
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-type, Accept, privatekey');
+    next();
+});
 exports.default = app;
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,14 +3,14 @@ import linksRouter from './routes/links';
 import cors from 'cors';
 
 const app = express();
+const allowedOrigins = ['http://www.prea.tk', 'http://api.prea.tk:3001'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
 app.use(express.json());
-app.use(cors());
+app.use(cors(options));
 app.use(linksRouter);
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-type, Accept, privatekey')
-    next();
-});
+
 
 export default app;
